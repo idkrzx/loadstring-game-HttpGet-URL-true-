8d1f894ed9e47eb3dc5e77e6876383df31e799e9
idkrzx <correofacil333344@gmail.com>
2026-03-12 02:58:44 -0600

    Create script.lua
    script.lua

---
+++ script.lua
@@ -0,0 +1,26 @@
+local vida = 100
+local modoDios = false
+
+function subirVida(cantidad)
+    vida = vida + cantidad
+    print("Vida:", vida)
+end
+
+function bajarVida(cantidad)
+    if modoDios then
+        print("Modo dios activo, no pierdes vida")
+    else
+        vida = vida - cantidad
+        print("Vida:", vida)
+    end
+end
+
+function activarModoDios()
+    modoDios = true
+    print("Modo dios ACTIVADO")
+end
+
+function desactivarModoDios()
+    modoDios = false
+    print("Modo dios DESACTIVADO")
+end
